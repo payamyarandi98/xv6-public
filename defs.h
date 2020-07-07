58231747b0f729aad2b17b12b7da8f255b85a15e
payamyarandi98 <yari.payam98@gmail.com>
2020-07-07 11:08:02 -0400

    add waitx and priority headers to defs.h

--- defs.h
+++ defs.h
@@ -121,6 +121,8 @@ int             wait(void);
 void            wakeup(void*);
 void            yield(void);
 int             pinfo(void);
+int             waitx(int*, int*)
+int             set_priority(int, int)
 
 // swtch.S
 void            swtch(struct context**, struct context*);
